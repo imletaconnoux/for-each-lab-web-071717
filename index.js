b94d880e692c0d2482e7be7b0d9c8417e77358c0
imletaconnoux <imletaconnoux@gmail.com>
2017-08-29 12:53:28 -0400

    passed tests

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach(function(element, index){
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  array = ["Isabelle", "Letaconnoux"]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+
+}
